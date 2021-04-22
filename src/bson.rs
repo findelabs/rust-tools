f077b2508cf044cd1057027590e67eb0aad2e6b3
Daniel Findell <55853529+Verticaleap@users.noreply.github.com>
2021-04-21 19:54:57 -0500

    Update bson.rs

--- src/bson.rs
+++ src/bson.rs
@@ -1,15 +1,13 @@
 use serde_json::{Map, Value};
 use bson::Document;
 
-pub fn to_doc(string: &str) -> Result<Document> {
+pub fn to_doc(string: &str) -> Result<Document, Box<dyn error::Error>> {
 
   let v: Map<String, Value> = match serde_json::from_str(value) {
     Ok(val) => {
-      log::info!("Successfully transformed data into json");
       val
     },
     Err(e) => {
-      log::info!("Got error converting data to json {}", e);
       return Err(e)
     }
   };
@@ -17,7 +15,6 @@ pub fn to_doc(string: &str) -> Result<Document> {
   let data = match bson::to_document(&v) {
     Ok(d) => d,
     Err(e) => {
-      log::info!("Got error converting {} to bson: {}", value, e);
       return Err(e)
     }
   };
