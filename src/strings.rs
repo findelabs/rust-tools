95622a0adfc6340ae982efd2603743bed8b79ef3
Daniel Findell <55853529+Verticaleap@users.noreply.github.com>
2021-04-21 19:26:25 -0500

    Update strings.rs

--- src/strings.rs
+++ src/strings.rs
@@ -3,12 +3,12 @@ use http::request::Parts;
 
 pub fn get_root_path(parts: &Parts) -> Option<String> {
   match parts.uri.path() {
-    "/" => "default".to_owned(),
+    "/" => Some("default".to_owned()),
     _ => {
       let stage_one: Vec<&str> = parts.uri.path().split("/").collect();    // Convert to array
       let stage_two = &stage_one[1..stage_one.len() - 1];                  // Remove the last path
       let stage_three = stage_two.join("_");                               // Join back with underscores
-      stage_three
+      Some(stage_three)
     }
   }
 }
