9b600884cee7e6120bcb49f77aa8f4b921ceae6a
Daniel Findell <55853529+Verticaleap@users.noreply.github.com>
2021-04-25 15:25:29 -0500

    Update bson.rs

--- src/bson.rs
+++ src/bson.rs
@@ -21,22 +21,3 @@ pub fn to_doc(value: &str) -> Result<Document, Box<dyn error::Error + Send + Syn
     }
   }
 }
-
-pub fn to_doc_vec(value: &str) -> Result<Vec<Document>, Box<dyn error::Error + Send + Sync>> {
-
-  let v: Vec<Map<String, Value>> = match serde_json::from_str(value) {
-    Ok(val) => {
-      val
-    },
-    Err(e) => {
-      return Err(Box::new(e))
-    }
-  };
-
-  match Document::try_from(v) {
-    Ok(d) => Ok(d),
-    Err(e) => {
-      return Err(Box::new(e))
-    }
-  }
-}
